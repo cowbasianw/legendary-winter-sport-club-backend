6524196e02afa08a3b10321d4adb00d97d7bf4e0
cowbasianw <77463732+cowbasianw@users.noreply.github.com>
2024-07-31 20:14:16 +0000

    deploy ready.

--- src/app.js
+++ src/app.js
@@ -9,13 +9,20 @@ app.use('/api/join', joinRouter);
 
 // Status route
 app.get('/status', (req, res) => {
-  res.status(200).send('Server is running successfully on port ${PORT}!');
+  res.status(200).send('Server is running successfully !');
 });
-
+app.get('/env', (req, res) => {
+  res.json({
+    PORT: process.env.PORT,
+    EMAIL_USER: process.env.EMAIL_USER,
+    // Add other environment variables you want to check
+  });
+});
 // Default route
 app.get('/', (req, res) => {
   res.status(200).send(`Server is running. Accessible domain http://localhost:5173, and https://legendarywintersports.com"`);
 });
+
 
 module.exports = app;
 
