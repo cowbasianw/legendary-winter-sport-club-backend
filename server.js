ceaf5a37e7aaf463a28b2cc69e06e858397239ec
cowbasianw <77463732+cowbasianw@users.noreply.github.com>
2024-07-31 19:23:45 +0000

    deploy ready.

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 require('dotenv').config();
-const app = require('./src/app');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = require('./src/app'); // Import app
 
 const PORT = process.env.PORT || 5001;
 
+// Configure CORS
+app.use(cors({
+  origin: 'https://legendarywintersports.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// Handle preflight requests
+app.options('*', cors({
+  origin: 'https://legendarywintersports.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    console.log(`http://localhost:${PORT}/`);
-    console.log("Email User:", process.env.EMAIL_USER);
-
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}/`);
+  console.log("Email User:", process.env.EMAIL_USER);
 });
--- src/app.js
+++ src/app.js
@@ -1,26 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const joinRouter = require('./routes/join.js');
 
 const app = express();
-const PORT = process.env.PORT || 5001; // Make sure PORT is defined here
 
-// Configure CORS
-app.use(cors({
-  origin: 'https://legendarywintersports.com',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-// Handle preflight requests
-app.options('*', cors({
-  origin: 'https://legendarywintersports.com',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-// Middleware
 app.use(bodyParser.json());
 app.use('/api/join', joinRouter);
 
@@ -31,13 +14,7 @@ app.get('/status', (req, res) => {
 
 // Default route
 app.get('/', (req, res) => {
-  res.status(200).send(`Server is running on port ${PORT}. Email User: ${process.env.EMAIL_USER}`);
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  console.log(`http://localhost:${PORT}/`);
-  console.log("Email User:", process.env.EMAIL_USER);
+  res.status(200).send(`Server is running.`);
 });
 
 module.exports = app;
