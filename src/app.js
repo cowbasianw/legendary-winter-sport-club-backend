fbb0e255e00b71197b507ef4778440b6d267a4ea
cowbasianw <77463732+cowbasianw@users.noreply.github.com>
2024-07-31 20:25:12 +0000

    deploy ready.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const joinRouter = require('./routes/join.js');
+const cors = require('cors');
 
 const app = express();
+
+// Configure CORS in app if not already set
+const corsOptions = {
+  origin: ['https://legendarywintersports.com', 'http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 
 app.use(bodyParser.json());
 app.use('/api/join', joinRouter);
