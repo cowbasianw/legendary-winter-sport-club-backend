ffa5d01217da7222300cdd8a0ac2d240b62b7d20
cowbasianw <77463732+cowbasianw@users.noreply.github.com>
2025-09-09 22:22:45 +0000

    email us app pass.

--- src/config/nodemailerconfig.js
+++ src/config/nodemailerconfig.js
@@ -2,18 +2,12 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.office365.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    service: 'gmail',
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    },
-
-    tls: {
-        rejectUnauthorized: true // Allow self-signed certificates, set to true in production
+        user: process.env.EMAIL_USER,  // your gmail address
+        pass: process.env.EMAIL_PASS   // your gmail app password or oauth token
     }
-})
+});
 
 const sendEmail = async (mailOptions) => {
     try {
